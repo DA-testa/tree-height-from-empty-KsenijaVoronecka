2a9e0fc8c2c4a7d837dea4b99e0475252de0ca1a
KsenijaVoronecka <124842373+KsenijaVoronecka@users.noreply.github.com>
2023-02-23 13:24:53 +0000

    updated

--- tree_height.py
+++ tree_height.py
@@ -1,25 +1,69 @@
-# python3
+# python3 tree_height.py
+# 4 -1 4 1 1
 
 import sys
 import threading
 
 
 def compute_height(n, parents):
-    # Write this function
     max_height = 0
+    if n < 0:
+        return max_height
+    # Write this function
+    child = n
     # Your code here
-    return max_height
+    for i in range(len(parents)):
+        child = parents[child]
+        max_height += 1
+        if child == -1:
+            break
+
+    # print(max_height)
+    return max_height if max_height > compute_height(n-1, parents) else compute_height(n-1, parents)
 
 
 def main():
     # implement input form keyboard and from files
+    command = input()
+
+    if "I" in command:
+        print("Enter a number of elements: ")
+        # input number of elements
+        elements_count = int(input())
+        # input values in one variable
+        print("Enter values: ")
+        values = input()
+
+    elif "F" in command:
+        # let user input file name to use, don't allow file names with letter a
+        print("Enter the file name: ")
+        fileName = input()
+
+        if "a" in fileName:
+            print("wrong file name")
+            return
+        
+        filePath = "./test/" + fileName
+        with open(filePath, mode="r") as fail:
+            # input number of elements
+            elements_count = int(fail.readline())
+            # input values in one variable
+            values = fail.readline()
+
+    else:
+        print("error")
+        return
+
+    # separate values with space, split these values in an array
+    values = values.split()
+    values = map(int, values)
+    values = list(values)
+
+    # call the function and output it's result
+    print("result: ", compute_height(elements_count - 1, values))
     
-    # let user input file name to use, don't allow file names with letter a
     # account for github input inprecision
     
-    # input number of elements
-    # input values in one variable, separate with space, split these values in an array
-    # call the function and output it's result
 
 
 # In Python, the default limit on recursion depth is rather low,
